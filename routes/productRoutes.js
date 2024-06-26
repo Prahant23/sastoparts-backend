5dc4c816e14375276010985a183b0c33a26543a6
PrashantBista <prashantbist64@gmail.com>
2024-06-26 20:09:28 +0545

    product routes added

---
+++ routes/productRoutes.js
@@ -0,0 +1,12 @@
+// importing 
+const router = require('express').Router();
+//making router
+const productControllers = require('../controllers/productControllers.js');
+const { authGuardAdmin, authGuard } = require('../middleware/authGuard.js');
+
+// exporting router
+router.post('/createProduct',authGuardAdmin,productControllers.createProduct);
+router.get('/getProduct',productControllers.getProducts);
+router.get('/getProductByUserId/:userId',authGuard,productControllers.getSingleProduct); //single product routes
+router.delete('/deleteProduct/:productId', authGuard, productControllers.deleteProduct);//deleteroute for products
+module.exports = router;

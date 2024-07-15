9b5037f0a0fcd34082b436f55b1b2c9f93ddcdc2
PrashantBista <prashantbist64@gmail.com>
2024-07-15 22:16:07 +0545

    routes added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,10 +4,10 @@ const productControllers = require('../controllers/productControllers');
 const { authGuardAdmin, authGuard } = require('../middleware/authGuard');
 
 router.post('/createProduct', authGuardAdmin, productControllers.createProduct);
-router.get('/getProduct', productControllers.getProducts);
+router.get('/getProducts', productControllers.getProducts);
 router.get('/getProductByUserId/:userId', authGuard, productControllers.getSingleProduct); // Consider renaming this to avoid confusion
 router.get('/get_product/:id', productControllers.getSingleProduct); // Route for fetching a single product
 router.put('/update_product/:id', authGuardAdmin, productControllers.updateProduct); // Route for updating a product
-router.delete('/deleteproduct/:id', authGuardAdmin, productControllers.deleteProduct); // Route for deleting a product
+router.delete('/products/:id', authGuardAdmin, productControllers.deleteProduct); // Route for deleting a product
 
 module.exports = router;
